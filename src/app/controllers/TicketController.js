9abc767e8e51799dc277818771093ec94e119ec3
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-09-23 14:19:42 -0300

    Update Ticket Search

--- src/app/controllers/TicketController.js
+++ src/app/controllers/TicketController.js
@@ -6,14 +6,37 @@ const { Op } = Sequelize;
 
 class TicketController {
   async show(req, res) {
-    const { visualizacao = 'todos', page = 0 } = req.query;
-    let { proto } = req.query;
+    const {
+      visualizacao = 'todos',
+      page = 0,
+      visualizacaoUser = 'todos',
+    } = req.query;
+    let { proto, de, para } = req.query;
     let aberto;
+    let fk_id_usuario;
+
+    if (!de) {
+      de = { [Op.like]: '%' };
+    } else {
+      de = { [Op.like]: `%${de}%` };
+    }
+
+    if (!para) {
+      para = { [Op.like]: '%' };
+    } else {
+      para = { [Op.like]: `%${para}%` };
+    }
 
     if (!proto) {
       proto = { [Op.like]: '%' };
     } else {
       proto = { [Op.like]: `%${proto}%` };
+    }
+
+    if (visualizacaoUser === 'todos') {
+      fk_id_usuario = { [Op.like]: '%' };
+    } else {
+      fk_id_usuario = visualizacaoUser;
     }
 
     if (visualizacao === 'todos') {
@@ -30,7 +53,14 @@ class TicketController {
     const offset = parseInt(page * 50, 10);
 
     const tickets = await Ticket.findAndCountAll({
-      where: { fk_id_dominio: user.fk_id_dominio, aberto, id: proto },
+      where: {
+        fk_id_dominio: user.fk_id_dominio,
+        aberto,
+        id: proto,
+        de,
+        para,
+        fk_id_usuario,
+      },
       include: [{ model: User, as: 'usuario', attributes: ['nome'] }],
       order: [['inicio', 'DESC']],
       limit: 50,
