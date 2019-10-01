4eb0b52f318c3e1068252c0ede7b4770f4719947
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-10-01 17:10:22 -0300

    Contact Search Filter

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,13 +1,36 @@
+import Sequelize from 'sequelize';
 import Contact from '../models/Contact';
 import ContactFields from '../models/ContactFields';
 
+const { Op } = Sequelize;
+
 class ContactController {
   async show(req, res) {
-    const contacts = await Contact.findAll({
+    const { page = 0 } = req.query;
+    let { did, descricao } = req.query;
+    const offset = parseInt(page * 50, 10);
+
+    if (!did) {
+      did = { [Op.like]: '%' };
+    } else {
+      did = { [Op.like]: `%${did}%` };
+    }
+
+    if (!descricao) {
+      descricao = { [Op.like]: '%' };
+    } else {
+      descricao = { [Op.like]: `%${descricao}%` };
+    }
+
+    const contacts = await Contact.findAndCountAll({
       where: {
         fk_id_dominio: req.user.id_dominio,
+        did,
+        descricao,
       },
       include: [ContactFields],
+      limit: 50,
+      offset,
     });
 
     res.json(contacts);
